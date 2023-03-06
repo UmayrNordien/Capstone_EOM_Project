25cae3a5c608aa11197c530b65839bbbc0fb981c
UmayrNordien <umayrnordien@gmail.com>
2023-03-06 16:58:09 +0200

    playing with navbar colors

---
+++ src/views/ContactView.vue
@@ -0,0 +1,16 @@
+<template>
+    <div>
+        <h1>Contact</h1>
+        <NavBar></NavBar>
+    </div>
+</template>
+
+<script>
+import NavBar from '../components/NavBar.vue'
+
+export default {
+    components: {
+        NavBar
+    }
+}
+</script>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,23 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1>Home</h1>
+    <HomePage></HomePage>
+    <NavBar></NavBar>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+
+import HomePage from '../components/HomePage.vue'
+import NavBar from '../components/NavBar.vue'
 
 export default {
-  name: 'HomeView',
   components: {
-    HelloWorld
+    HomePage,
+    NavBar
   }
 }
 </script>
+
+<style scoped>
+</style>
