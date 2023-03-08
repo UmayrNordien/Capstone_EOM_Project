6c7e277ccfb69945d466fea895928a69cb5d96e2
UmayrNordien <umayrnordien@gmail.com>
2023-03-08 13:13:21 +0200

    index.js

---
+++ api/index.js
@@ -0,0 +1,35 @@
+const express = require('express');
+// route
+const route = require('./controller/index.js');
+// cors
+const cors = require('cors');
+// port
+const port = parseInt(process.env.PORT) || 4000;
+// Express app
+const app = express();
+const cookieParser = require('cookie-parser');
+const {ErrorHandling} = require('./middleware/ErrorHandling')
+
+
+app.use((req, res, next)=> {
+        res.header('Access-Control-Allow-Origin', '*')
+        res.header("Access-Control-Allow-Credentials", "true")
+        res.header("Access-Control-Allow-Methods", "*")
+        res.header("Access-Control-Allow-Headers", "*")
+        next();
+});
+app.use(route);
+app.use(
+    cors(),
+    cookieParser(),
+    express.json(),
+    express.urlencoded({extended: false})
+)
+
+// The Server is running
+app.listen(port, ()=> {
+    console.log(`Server is running @ port ${port}`);
+});
+
+// The Handling of all errors
+app.use(ErrorHandling);
