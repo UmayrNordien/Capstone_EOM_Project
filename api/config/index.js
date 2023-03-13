bb9c8590ca1045a63f00afc24818956ab64ae560
UmayrNordien <umayrnordien@gmail.com>
2023-03-13 16:37:52 +0200

    inactivity error fix - test in config

--- api/config/index.js
+++ api/config/index.js
@@ -1,7 +1,14 @@
 require('dotenv').config();
 const { createPool } = require('mysql');
-
+//Error: Handshake inactivity timeout
+//https://stackoverflow.com/questions/35553432/error-handshake-inactivity-timeout-in-node-js-mysql-module
 let connection = createPool({
+    
+    connectionLimit : 1000,
+    connectTimeout  : 60 * 60 * 1000,
+    acquireTimeout  : 60 * 60 * 1000,
+    timeout         : 60 * 60 * 1000,
+
     host: process.env.HOST,
     user: process.env.USER,
     password: process.env.PASSWORD,
