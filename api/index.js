57f13705356e0aa73f31732a6ce9b7232629d0ce
UmayrNordien <umayrnordien@gmail.com>
2023-03-13 16:06:02 +0200

    render link inserted into index.js

--- api/index.js
+++ api/index.js
@@ -12,7 +12,7 @@ const {ErrorHandling} = require('./middleware/ErrorHandling')
 
 
 app.use((req, res, next)=> {
-        res.header('Access-Control-Allow-Origin', '*')
+        res.header('Access-Control-Allow-Origin', 'https://capstone-ecommerce.onrender.com/')
         res.header("Access-Control-Allow-Credentials", "true")
         res.header("Access-Control-Allow-Methods", "*")
         res.header("Access-Control-Allow-Headers", "*")
