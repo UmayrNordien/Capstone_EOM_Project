aa31eab1f1eaf6f85e8d911bfe4aebaa95c21dcc
UmayrNordien <umayrnordien@gmail.com>
2023-03-14 14:38:33 +0200

    .env update

--- api/config/index.js
+++ api/config/index.js
@@ -12,7 +12,7 @@ let connection = createPool({
     host: process.env.HOST,
     user: process.env.USER,
     password: process.env.PASSWORD,
-    port: process.env.PORT,
+    port: process.env.dbPORT,
     database: process.env.DB,
     multipleStatements: true
 });
